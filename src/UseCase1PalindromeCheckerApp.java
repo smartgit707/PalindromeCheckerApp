06942b63dc57b058d8009a1548716ad1cf42a0b9
smartgit707 <manmohan6145@gmail.com>
2026-02-20 09:19:50 +0530

    UC1: Add welcome message module

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        // Displaying the system initialization messages
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
